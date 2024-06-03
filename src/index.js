406fab0dd57416c055ca3fcb01f2055842171a76
thepralad <prgurung04@gmail.com>
2024-06-04 01:00:27 +0530

    feat: Add functionality to add and display books in the library
    The code changes include adding functions to add books to the library and display all the books in the library. This will allow users to easily add new books and view the existing books in the library.
    
    Note: The recent user and repository commits do not provide any additional relevant information for the commit message.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import { Book } from "./book";
 
+let library = new Array();
 
+function addBook(title, author, pages){
+    const newBook = new Book(title,author,pages)
+    library.push(newBook);
+}
+
+function displayBooks(){
+    console.table(library);
+}
+addBook('How to solve it', 'George Polya', 140);
+addBook('Hyper Focus', 'James Author', 563);
+displayBooks();
+
+export { library };
