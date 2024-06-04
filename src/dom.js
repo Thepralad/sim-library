4f15b191723c88068b57a9f9d50245772afa02c8
thepralad <prgurung04@gmail.com>
2024-06-04 14:02:21 +0530

    Refactor code to use class for book and update import statement in dom.js

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,36 @@
-import { library } from "./book.js";
+import { library } from "./index.js";
 
+
+
+function displayLibrarayDom(){
+    const container = document.getElementById('container');
+    for (let i = 0; i < library.length; i++) {
+        //creating elements.
+        const book = document.createElement('div');
+        const title = document.createElement('h3');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+        const hr = document.createElement('hr');
+        //adding class to elements.
+        title.classList.add('title');
+        author.classList.add('author');
+        pages.classList.add('pages');
+        book.classList.add('book');
+
+        title.textContent = library[i].title;
+        author.textContent = library[i].author;
+        pages.textContent = `${library[i].pages} pages`;
+
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(hr);
+
+        container.appendChild(book);
+        
+    }
+    
+
+}
+
+export { displayLibrarayDom }
